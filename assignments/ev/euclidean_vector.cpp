60f41fc05f8623d93d2d0bdaa83c720115b746c0
Nevin Suriyaaratchie <nevinsuri@hotmail.com>
2019-07-09 06:08:02 +1000

    lol nice

--- assignments/ev/euclidean_vector.cpp
+++ assignments/ev/euclidean_vector.cpp
@@ -1,3 +1,9 @@
 #include "assignments/ev/euclidean_vector.h"
 
 #include <algorithm>  // Look at these - they are helpful https://en.cppreference.com/w/cpp/algorithm
+#include <utility>
+
+EuclideanVector::EuclideanVector(EuclideanVector&& o) noexcept :
+magnitudes_{std::move(o.magnitudes_)}, size_{o.size_} {
+  o.size_ = 0;
+}
--- assignments/ev/euclidean_vector.h
+++ assignments/ev/euclidean_vector.h
@@ -1,13 +1,17 @@
-// TODO(you): Include header guards
+#ifndef ASSIGNMENTS_EV_EUCLIDEAN_VECTOR_H_
+#define ASSIGNMENTS_EV_EUCLIDEAN_VECTOR_H_
 
 #include <exception>
 #include <string>
 #include <memory>
+#include <vector>
+#include <utility>
+
 
 class EuclideanVectorError : public std::exception {
  public:
   explicit EuclideanVectorError(const std::string& what) : what_(what) {}
-  const char* what() const noexcept{ return what_.c_str(); }
+  const char* what() const noexcept { return what_.c_str(); }
  private:
   std::string what_;
 };
@@ -16,8 +20,21 @@ class EuclideanVector {
  public:
   explicit EuclideanVector(int i);
   friend std::ostream& operator<<(std::ostream& os, const EuclideanVector& v);
-  // TODO(you): add more
+  explicit EuclideanVector(int dimensions = 0);
+  EuclideanVector(int dimensions, double magnitude);
+  EuclideanVector(std::vector<double>::const_iterator start,
+                  std::vector<double>::const_iterator end);
+  EuclideanVector(const EuclideanVector&);
+  EuclideanVector(EuclideanVector&&);
+
+  EuclideanVector operator=(EuclideanVector&& o) noexcept {
+    magnitudes_ = std::move(o.magnitudes_);
+    size_ = o.size_;
+    o.size_ = 0;
+  }
  private:
   std::unique_ptr<double[]> magnitudes_;
-  // TODO(you): add more
+  int size_;
 };
+
+#endif  // ASSIGNMENTS_EV_EUCLIDEAN_VECTOR_H_
